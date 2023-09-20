d93fd9fa08eb64d08c53ecf4e34ba18a64ab51d4
meet vasoya <meetvasoya37@gmail.com>
2023-09-20 11:20:18 +0530

    Day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,10 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_declarations, use_key_in_widget_constructors, camel_case_types, avoid_print, non_constant_identifier_names
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_declarations, use_key_in_widget_constructors, camel_case_types, avoid_print, non_constant_identifier_names, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_application_1/models/items.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
-import 'package:flutter_application_1/widgets/item_widget.dart';
 
 class homepage extends StatefulWidget {
   @override
@@ -23,9 +22,9 @@ class _homepageState extends State<homepage> {
   }
 
   loaddata() async {
-    await Future.delayed(Duration(seconds: 3));
+    await Future.delayed(Duration(seconds: 2));
     final catalogJson =
-        await rootBundle.loadString("assets/files/catalog.json")        ;
+        await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
     var Productsdata = decodedData["Products"];
     CatalogModel.items = List.from(Productsdata)
@@ -45,12 +44,44 @@ class _homepageState extends State<homepage> {
           )),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)?  ListView.builder(
-          itemCount: CatalogModel.items!.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: CatalogModel.items![index]);
-          },
-        ):Center(child: CircularProgressIndicator(),),
+        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16
+                    ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items![index];
+                  return Card
+                  (clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder
+                  (borderRadius: BorderRadius.circular(10)),  
+                    child: GridTile
+                  (header: Container(
+                    child: Text(item.name,style: TextStyle(color: Colors.white),),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                    ),
+                    ),
+                    child: Image.network(item.image),
+                    footer: Container(
+                    child: Text(item.price.toString(),
+                    style: TextStyle(color: Colors.white),),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                    ),
+                    ),
+                    )
+                  );
+                },
+                itemCount: CatalogModel.items?.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: Mydrawer(),
     );
